fe42c99f1e704ee5e31e04bf0f13afec4be1fb8b
PROFESSOR <66514907+Adithyan1133-ctrl@users.noreply.github.com>
2020-07-04 18:56:35 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -324,7 +324,7 @@ async def youtube_dl_call_back(bot, update):
                 i = 0
                 caption = "© @TheUnusualPsychopath"
                 if is_w_f:
-                    caption = "/upgrade to Plan D to remove the watermark\n© @AnyDLBot"
+                    caption = "My owner @TheUnusualPsychopath"
                 for image in images:
                     if os.path.exists(image):
                         if i == 0:
